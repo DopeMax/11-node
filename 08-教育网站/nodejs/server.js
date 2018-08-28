972b713b58dc498566858aab5804d3cac06145fa
DopeMax <amingccc@gmail.com>
2018-08-28 22:08:27 +0800

    讲课代码

--- 08-教育网站/nodejs/server.js
+++ 08-教育网站/nodejs/server.js
@@ -14,6 +14,7 @@ server.listen(8080);
 
 //1.获取请求数据
 //get自带
+server.use(bodyParser.urlencoded());
 server.use(multerObj.any());
 
 //2.cookie、session
@@ -36,11 +37,8 @@ server.set('views', 'template');
 server.set('view engine', 'html');
 
 //4.route
-// server.use('/article/', require('./route/1.js')());
-// server.use('/blog/', require('./route/2.js')());
-
- server.use('/', require('./route/web.js')());
- server.use('/admin/', require('./route/admin.js')());
+server.use('/', require('./route/web')());
+server.use('/admin/', require('./route/admin')());
 
 //5.default：static
 server.use(static('./static/'));
